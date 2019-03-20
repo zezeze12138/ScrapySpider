ab40fdbcb7767e024a49e1e071e84ddfcd893f5f
zengqzhen <954723922@qq.com>
2019-03-20 18:56:22 +0800

    解析xml文件更新

--- .idea/DOMparse/parse.py
+++ .idea/DOMparse/parse.py
@@ -2,33 +2,35 @@
 from xml.dom.minidom import parse
 from editScrapy import EditClass
 #xmlStr = open('E:\scrapy.xml','r')
-doc=parse('E:\scrapy.xml')
+doc=parse('F:\ideaSP\ScrapySpider\.idea\DOMparse\scrapy.xml')
 root=doc.documentElement
 
 editClass = EditClass.EditClass()
 
-#spider获取
+#xml中获取spider
 spider = root.getElementsByTagName("spider")[0]
 name = spider.getElementsByTagName('name')[0]
 start_urls = spider.getElementsByTagName('start_urls')[0]
 allowed_domains = spider.getElementsByTagName('allowed_domains')[0]
 start_url = []
 allowed_domain = []
-#for url in start_urls:
 start_url.append(start_urls.childNodes[0].data)
-#for allowed in allowed_domains:
 allowed_domain.append(allowed_domains.childNodes[0].data)
+
+#编写spider类初始化头部
 print (editClass.addSpiderClass("spiderTest", name.childNodes[0].data, allowed_domain, start_url))
-print(name.childNodes[0].data)
-print(allowed_domains.childNodes[0].data)
-print(start_urls.childNodes[0].data)
+#编写parse解析方法
+print (editClass.addDefAndparam("parse",["self","response"]))
+#编写解析方法头部
+print (editClass.addVar(3,"item = Demo1Item()"))
+
 parse = spider.getElementsByTagName('parse')[0]
 xpaths = parse.getElementsByTagName('xpath')
 for xpath in xpaths:
     print (xpath.getAttribute("item"))
     print (xpath.childNodes[0].data)
 
-#item获取
+#xml中获取item
 items=root.getElementsByTagName("items")[0]
 for item in items.getElementsByTagName("item"):
     print (item.childNodes[0].data)
