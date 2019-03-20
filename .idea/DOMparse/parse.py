de7c84dced0e3b3cf46483fa185e3ab6e47323d9
zengqzhen <954723922@qq.com>
2019-03-20 18:22:36 +0800

    进行xml文件解析测试

--- .idea/DOMparse/parse.py
+++ .idea/DOMparse/parse.py
@@ -1,43 +1,35 @@
-class DomXmlParse():
+#coding=utf-8
+from xml.dom.minidom import parse
+from editScrapy import EditClass
+#xmlStr = open('E:\scrapy.xml','r')
+doc=parse('E:\scrapy.xml')
+root=doc.documentElement
 
+editClass = EditClass.EditClass()
 
+#spider获取
+spider = root.getElementsByTagName("spider")[0]
+name = spider.getElementsByTagName('name')[0]
+start_urls = spider.getElementsByTagName('start_urls')[0]
+allowed_domains = spider.getElementsByTagName('allowed_domains')[0]
+start_url = []
+allowed_domain = []
+#for url in start_urls:
+start_url.append(start_urls.childNodes[0].data)
+#for allowed in allowed_domains:
+allowed_domain.append(allowed_domains.childNodes[0].data)
+print (editClass.addSpiderClass("spiderTest", name.childNodes[0].data, allowed_domain, start_url))
+print(name.childNodes[0].data)
+print(allowed_domains.childNodes[0].data)
+print(start_urls.childNodes[0].data)
+parse = spider.getElementsByTagName('parse')[0]
+xpaths = parse.getElementsByTagName('xpath')
+for xpath in xpaths:
+    print (xpath.getAttribute("item"))
+    print (xpath.childNodes[0].data)
 
-    def getDom():
-        domStr='''
-        <collection shelf="New Arrivals">
-    <movie title="Enemy Behind">
-       <type>War, Thriller</type>
-       <format>DVD</format>
-       <year>2003</year>
-       <rating>PG</rating>
-       <stars>10</stars>
-       <description>Talk about a US-Japan war</description>
-    </movie>
-    <movie title="Transformers">
-       <type>Anime, Science Fiction</type>
-       <format>DVD</format>
-       <year>1989</year>
-       <rating>R</rating>
-       <stars>8</stars>
-       <description>A schientific fiction</description>
-    </movie>
-       <movie title="Trigun">
-       <type>Anime, Action</type>
-       <format>DVD</format>
-       <episodes>4</episodes>
-       <rating>PG</rating>
-       <stars>10</stars>
-       <description>Vash the Stampede!</description>
-    </movie>
-    <movie title="Ishtar">
-       <type>Comedy</type>
-       <format>VHS</format>
-       <rating>PG</rating>
-       <stars>2</stars>
-       <description>Viewable boredom</description>
-    </movie>
-    </collection>
-        '''
-        return domStr
+#item获取
+items=root.getElementsByTagName("items")[0]
+for item in items.getElementsByTagName("item"):
+    print (item.childNodes[0].data)
 
-    print(getDom())
